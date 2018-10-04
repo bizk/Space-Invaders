70984f17aee20e268ce6400d2c62b338921818c4
bizkuit <santiago.yanzon1999@gmail.com>
2018-10-04 17:22:13 +0200

    ImplementacionST-V0.2
    Agruege movimiento, y le di un poco mas de vuelta de rosca a algunas de las cosas

---
+++ src/tests/pruebasMixtas.java
@@ -0,0 +1,27 @@
+package tests;
+
+import java.util.Scanner;
+
+import app.Juego;
+
+public class pruebasMixtas {
+	// Test 01 - leer teclado de forma continua
+	public static void main(String[] args) {
+		Juego juego = new Juego();
+		juego.nuevoJuego();
+		
+		Scanner scanner = new Scanner(System.in);	
+		while(true) {
+			System.out.println("Escribi algo mono:");
+			String input = scanner.nextLine();
+			juego.movimientoJugador(input);
+			if("q".equals(input)) {
+				juego.terminarJuego();
+				break;
+			}
+			//juego.siguienteNivel();
+		}
+		
+		System.out.println("\n### Programa terminado con exito ###");
+	}
+}
